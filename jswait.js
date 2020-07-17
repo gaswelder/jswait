34c398d48dc38989688bb62543364f02f77382b1
gasweld <gasweld@gmail.com>
2020-07-17 23:30:02 +0300

    Add more cases

--- jswait.js
+++ jswait.js
@@ -3,25 +3,31 @@ const acornwalk = require("acorn-walk");
 const escodegen = require("escodegen");
 const fs = require("fs");
 
-const convertModule = src => {
+const convertModule = (src) => {
   const tree = acorn.parse(src);
   acornwalk.simple(tree, {
     FunctionDeclaration(node) {
       node.async = true;
     },
+    FunctionExpression(node) {
+      node.async = true;
+    },
     ArrowFunctionExpression(node) {
       node.async = true;
+    },
+    MethodDefinition(node) {
+      node.value.async = node.kind != "constructor";
     },
     CallExpression(node) {
       const argument = { ...node };
       node.type = "AwaitExpression";
       node.argument = argument;
-    }
+    },
   });
   return `(async () => {${escodegen.generate(tree)}})();`;
 };
 
-const main = args => {
+const main = (args) => {
   const src = fs.readFileSync(args[1]);
   const js = convertModule(src);
   eval(js);
